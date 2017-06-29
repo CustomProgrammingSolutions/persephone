eeb45952890f94038018bb4de07d618330986fcd
Oliver Adams <oliver.adams@gmail.com>
2017-06-29 12:22:21 +1000

    added script to derive phoneme inventory.

---
+++ scripts/phoneme_inv.py
@@ -0,0 +1,16 @@
+""" Derives a phoneme set from [preprocessed] transcriptions in a given directory."""
+
+import os
+import sys
+
+PATH = sys.argv[1]
+
+PHONEMES = set()
+for root, dirn, filenames in os.walk(PATH):
+    for filename in filenames:
+        file_path = os.path.join(root, filename)
+        with open(file_path) as f:
+            for line in f:
+                PHONEMES = PHONEMES.union(set(line.split()))
+
+print(PHONEMES)

544dc2b7aeff41cbe34abce1589b2a82c6f03603
Oliver Adams <oliver.adams@gmail.com>
2017-09-20 17:33:15 +1000

    results presentation tweak.

--- src/results.py
+++ src/results.py
@@ -37,7 +37,11 @@ def format(exp_paths,
     print("Test PER", round_items(test_pers))
     print("Test TER", round_items(test_ters))
 
+    print("TONES")
     for item in zip([128,256,512,1024,2048], test_ters):
+        print("(%d, %f)" % item)
+    print("PHONEMES")
+    for item in zip([128,256,512,1024,2048], test_pers):
         print("(%d, %f)" % item)
 
 def test_results(exp_path, phones, tones):
